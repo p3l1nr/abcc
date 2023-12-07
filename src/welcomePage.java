d9b47ac36481e237a00f73e14a9a745fda8a7287
Robert <pelin.robert7@gmail.com>
2023-12-07 23:27:06 +0100

    dasdasdadasdad

---
+++ src/welcomePage.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class welcomePage extends JFrame{
+JFrame f;
+welcomePage(){
+
+JButton b=new JButton("click");
+JPanel panel = new JPanel();
+b.setBounds(100,100,100, 100);
+    b.setBorderPainted(false);
+    b.setFocusPainted(false);
+    b.setContentAreaFilled(false);
+add(panel);
+panel.add(b);
+panel.setSize(400,500);
+panel.setBackground(Color.black);
+panel.setVisible(true);
+panel.setLayout(null);
+
+
+setSize(400,500);
+setLayout(null);
+setVisible(true);
+setBackground(Color.black);
+setLocationRelativeTo(null);
+
+
+    b.addActionListener(e -> new GameFrame());
+}
+
+public static void main(String[] args) {
+new GameFrame();
+}
+}
