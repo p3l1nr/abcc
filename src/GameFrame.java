56d6fac84245d8d97147d0c5de23e8336a9a4f7f
Robert <pelin.robert7@gmail.com>
2023-12-08 12:23:03 +0100

    welcomePage with esc button

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,15 +5,15 @@ public class GameFrame extends JFrame {
 
     GamePanel panel;
 
-    GameFrame() {
-        panel = new GamePanel();
-        this.add(panel);
-        this.setTitle("Pong Game");
-        this.setResizable(false);
-        this.setBackground(Color.black);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
+    GameFrame( ) {
+        panel = new GamePanel( );
+        this.add( panel );
+        this.setTitle( "Pong Game" );
+        this.setResizable( false );
+        this.setBackground( Color.white );
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        this.pack( );
+        this.setVisible( true );
+        this.setLocationRelativeTo( null );
     }
 }
